2e206b6b58141069fb5865cdaef0314e8bef7ad2
Irina Saparina <saparina@users.noreply.github.com>
2018-01-31 23:36:35 +0300

    Update run.sh

--- run.sh
+++ run.sh
@@ -6,17 +6,21 @@ cd /workspace/OpenNMT-py
 # Preprocessing
 python ~/split_parallel_corpus.py
 
+python ~/embeddings.py
+
 head -n 20 ~/data/source.train.txt > ~/data/source.valid.txt
 head -n 20 ~/data/target.train.txt > ~/data/target.valid.txt
 
 cp /data/input.txt ~/data/source.test.txt
 cp /data/corpus1.txt ~/data/source.mono.txt
-head -n 50000 /data/corpus2.txt > ~/data/target.mono.txt
+head -n 20000 /data/corpus2.txt > ~/data/target.mono.txt
 
 # Forward Translation
 python preprocess.py -train_src ~/data/source.train.txt -train_tgt ~/data/target.train.txt -valid_src ~/data/source.valid.txt -valid_tgt ~/data/target.valid.txt -save_data ~/data/demo
 
-python train.py -data ~/data/demo -save_model ~/demo-model -epochs 13 -batch_size=64 -gpuid 0
+./tools/embeddings_to_torch.py -emb_file "src_vectors" -dict_file ~/data/demo.vocab.pt -output_file ~/data/embeddings
+
+python train.py -data ~/data/demo -save_model ~/demo-model -epochs 13 -batch_size=64 -gpuid 0 -pre_word_vecs_enc ~/data/embeddings.enc.pt
 
 NEWEST_MODEL=$(ls -Ct ~ | awk '{print $1}' | head -n1)
 python translate.py -model ~/$NEWEST_MODEL -src ~/data/source.mono.txt -output ~/data/target.backtranslated.txt -replace_unk -verbose -gpu 0
