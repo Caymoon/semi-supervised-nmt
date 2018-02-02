f8defc3d068876be00ed4a55367640d58791249d
Irina Saparina <saparina@users.noreply.github.com>
2018-02-02 23:36:31 +0300

    Update run.sh

--- run.sh
+++ run.sh
@@ -15,14 +15,14 @@ head -n 20 ~/data/target.train.txt > ~/data/target.valid.txt
 
 cp /data/input.txt ~/data/source.test.txt
 cp /data/corpus1.txt ~/data/source.mono.txt
-head -n 50000 /data/corpus2.txt > ~/data/target.mono.txt
+head -n 20000 /data/corpus2.txt > ~/data/target.mono.txt
 
 # Forward Translation
 python preprocess.py -train_src ~/data/target.train.txt -train_tgt ~/data/source.train.txt -valid_src ~/data/target.valid.txt -valid_tgt ~/data/source.valid.txt -save_data ~/data/demo_tr
 
 python tools/embeddings_to_torch.py -emb_file ~/data/all_vectors -dict_file ~/data/demo_tr.vocab.pt -output_file ~/data/embeddings
 
-python train.py -data ~/data/demo_tr  -save_model ~/demo-model -epochs 50 -batch_size=64 -gpuid 0 -word_vec_size 300 -encoder_type brnn -pre_word_vecs_enc ~/data/embeddings.enc.pt -global_attention mlp -optim adam
+python train.py -data ~/data/demo_tr  -save_model ~/demo-model -epochs 70 -batch_size=64 -gpuid 0 -word_vec_size 300 -encoder_type brnn -pre_word_vecs_enc ~/data/embeddings.enc.pt -optim adam
 
 NEWEST_MODEL=$(ls -Ct ~ | awk '{print $1}' | head -n1)
 python translate.py -model ~/$NEWEST_MODEL -src ~/data/target.mono.txt -output ~/data/source.backtranslated.txt -replace_unk -verbose -gpu 0
@@ -39,7 +39,7 @@ python preprocess.py -train_src ~/data/source.train.txt -train_tgt ~/data/target
 
 python tools/embeddings_to_torch.py -emb_file ~/data/all_vectors -dict_file ~/data/demo.vocab.pt -output_file ~/data/embeddings
 
-python train.py -data ~/data/demo -save_model ~/model  -epochs 50 -batch_size=64 -gpuid 0 -word_vec_size 300 -encoder_type brnn -pre_word_vecs_enc ~/data/embeddings.enc.pt -global_attention mlp -optim adam
+python train.py -data ~/data/demo -save_model ~/model  -epochs 70 -batch_size=64 -gpuid 0 -word_vec_size 300 -encoder_type brnn -pre_word_vecs_enc ~/data/embeddings.enc.pt -optim adam
 
 NEWEST_MODEL=$(ls -Ct ~ | awk '{print $1}' | head -n1)
 python translate.py -model ~/$NEWEST_MODEL -src ~/data/source.test.txt -output /output/output.txt -replace_unk -verbose -gpu 0
